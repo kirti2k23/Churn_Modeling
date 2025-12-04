8cc3876a93def0652e4863a8b092cc6d9673576b
Kirti Verma <Kv.edu14@gmail.com>
2025-12-04 08:11:22 +0530

    Created logger

---
+++ logger.py
@@ -0,0 +1,18 @@
+import logging
+from datetime import datetime
+import os
+
+Log_File = f'log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log'
+log_path = os.path.join(os.getcwd(),"Logs",Log_File)
+os.makedirs(log_path, exist_ok = True)
+
+Log_File_Path = os.path.join(log_path,Log_File)
+
+logging.basicConfig(
+    filename= Log_File_Path,
+    format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+)
+
+if __name__ == "__main__":
+    logging.info("Logging started")
