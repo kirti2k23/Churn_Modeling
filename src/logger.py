7ba2b5fc3c3f02bf6064ef2e53bba2ae9f73c633
Kirti Verma <Kv.edu14@gmail.com>
2025-12-05 07:15:11 +0530

    "logger created"

--- src/logger.py
+++ src/logger.py
@@ -2,8 +2,8 @@ import logging
 from datetime import datetime
 import os
 
-Log_File = f'log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log'
-log_path = os.path.join(os.getcwd(),"Logs",Log_File)
+Log_File = f"log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+log_path = os.path.join(os.getcwd(),"Logs")
 os.makedirs(log_path, exist_ok = True)
 
 Log_File_Path = os.path.join(log_path,Log_File)
